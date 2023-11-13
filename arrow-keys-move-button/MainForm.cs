e5fdd08164ef89ec38c36c62d28e9fe9203b56ba
Tom Gregor <tgregor@ivsoftware.com>
2023-11-13 07:01:26 -0700

    RC

--- arrow-keys-move-button/MainForm.cs
+++ arrow-keys-move-button/MainForm.cs
@@ -4,20 +4,48 @@ using System.Windows.Forms.VisualStyles;
 
 namespace arrow_keys_move_button
 {
-    public partial class MainForm : Form, IMessageFilter
+    public partial class MainForm : Form
     {
         public MainForm()
         {
             InitializeComponent();
+            buttonDeselect.Click += (sender, e) =>
+            {
+                foreach (var pb in Controls.OfType<PictureBoxEx>())
+                {
+                    pb.BorderStyle = BorderStyle.None;
+                }
+            };
+        }
+    }
+
+    class PictureBoxEx : PictureBox, IMessageFilter
+    {
+        public PictureBoxEx()
+        {
             Application.AddMessageFilter(this);
             Disposed += (sender, e) => Application.RemoveMessageFilter(this);
         }
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            if (ModifierKeys != Keys.Control)   // Allow multiple select.
+            {
+                foreach (var pb in Parent.Controls.OfType<PictureBoxEx>())
+                {
+                    pb.BorderStyle = BorderStyle.None;
+                }
+            }
+            BorderStyle = BorderStyle.FixedSingle;
+        }
+        private const int WM_KEYDOWN = 0x0100;
         public bool PreFilterMessage(ref Message m)
         {
-            if (m.Msg == WM_KEYDOWN)
+            if ((m.Msg == WM_KEYDOWN) && (BorderStyle == BorderStyle.FixedSingle))
             {
+
                 var e = new KeyEventArgs(((Keys)m.WParam) | Control.ModifierKeys);
-                OnHotKeyPreview(FromHandle(m.HWnd), e);
+                OnKeyDown(e);
                 return (e.Handled);
             }
             return false;
@@ -27,27 +55,19 @@ namespace arrow_keys_move_button
             base.OnKeyDown(e);
             switch (e.KeyData)
             {
-                case Keys.Left: onMoveLeft(); e.Handled = true; break;
-                case Keys.Up: onMoveUp(); e.Handled = true; break;
-                case Keys.Right: onMoveRight(); e.Handled = true; break;
-                case Keys.Down: onMoveDown(); e.Handled = true; break;
+                case Keys.Left:
+                    Location = new Point(Location.X - 15, Location.Y);
+                    break;
+                case Keys.Up:
+                    Location = new Point(Location.X, Location.Y - 15);
+                    break;
+                case Keys.Right:
+                    Location = new Point(Location.X + 15, Location.Y);
+                    break;
+                case Keys.Down:
+                    Location = new Point(Location.X, Location.Y + 15);
+                    break;
             }
         }
-
-        private void onMoveLeft() =>
-            pictureBox1.Location = new Point(pictureBox1.Location.X - 15, pictureBox1.Location.Y);
-
-        private void onMoveUp() =>
-            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 15);
-
-        private void onMoveRight() =>
-            pictureBox1.Location = new Point(pictureBox1.Location.X + 15,
-                pictureBox1.Location.Y);
-        private void onMoveDown() =>
-            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 15);
-
-
-        private const int WM_KEYDOWN = 0x0100;
-        private const int WM_HOTKEY = 0x0312;
     }
 }
